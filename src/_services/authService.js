cff340873143ae62b6622e0f9d847af5c6699bd8
Luis Guerrero Roquett <luisguerreroroquett@gmail.com>
2023-05-14 14:20:39 +0200

    add authService

---
+++ src/_services/authService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { global } from "../_global/global";
+
+const authService = {};
+
+//Login de usuarios
+authService.login = async (credentials) => {
+  const body = {
+    email: credentials.email,
+    password: credentials.password,
+  };
+
+  return (await axios.post(global.BASE_URL + "/auth/login", body)).data;
+};
+
+//registro de usuarios
+authService.registerUser = async (data) => {
+
+  const body = {
+    nombre: data.nombre,
+    apellidos: data.apellidos,
+    email: data.email,
+    telefono: data.telefono,
+    password: data.password,
+  };
+
+
+  return (await axios.post(global.BASE_URL + `/auth/register/`, body))
+    .data;
+}
+
+
+export default authService
