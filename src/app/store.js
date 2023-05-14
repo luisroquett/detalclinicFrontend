e39b2c1e99f44a4c4f04689662dd5911d7813b14
Luis Guerrero Roquett <luisguerreroroquett@gmail.com>
2023-05-14 16:11:51 +0200

    created utils and store

---
+++ src/app/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authentication/authSlice";
+
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import sessionStorage from "redux-persist/lib/storage/session";
+
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["auth"], //hay que poner la key
+};
+
+const authPersistConfig = {
+  key: "auth",
+  storage: sessionStorage,
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
