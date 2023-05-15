c84a70188a1bd0f4c693f574c648e2a6e2e667d2
Luis Guerrero Roquett <luisguerreroroquett@gmail.com>
2023-05-15 23:38:46 +0200

    add file footer

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/";
+import UsersList from "./user-list";
+import TablePagination from "./table-pagination";
+import DataListTable from "./data-list-table";
+import Footer from "./footer";
+
+export { Header, UsersList, TablePagination, DataListTable, Footer };
