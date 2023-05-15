1f4be1f6b7fb77eb40e15938ca9128952ff2146d
Luis Guerrero Roquett <luisguerreroroquett@gmail.com>
2023-05-15 15:59:11 +0200

    export function page not found

---
+++ src/containers/page-not-found/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PageNotFound.scss";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <h2>Error 404</h2>
+    </div>
+  );
+}
